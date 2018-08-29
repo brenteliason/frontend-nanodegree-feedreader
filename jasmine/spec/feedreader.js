fa0c2e9e1188baca9f068393ba401f0d9c6a276f
Brent <brenteliason@gmail.com>
2018-08-29 11:34:54 -0400

    Passes all tests

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -56,20 +56,49 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "The menu" */
-
+    describe('The Menu Suite', function() {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
-
+         describe("The menu ", function () {
+           it("is hidden by default", function () {
+             let bodyTag = document.getElementsByTagName('body')[0];
+             expect(bodyTag.classList.contains("menu-hidden")).toBe(true);
+           })
+         })
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+        describe("The menu", function () {
+          it("changes visibility when clicked, and again when clicked again", function () {
+            //simulate click - menu should be visible
+            //let bodyTag = document.getElementsByTagName('body')[0];
+            //console.log("Body tag equals: " + bodyTag);
+            //bodyTag.click();
+            let menu = document.querySelector('.menu-icon-link')
+            menu.click();
+            //console.log("Simulated one mouseclick")
+            let bodyTag = document.getElementsByTagName('body')[0];
+            //let bodyTag = document.getElementsByTagName('body')[0];
+            //console.log("Body class list equals: " + bodyTag.classList);
+            expect(bodyTag.classList.contains("menu-hidden")).toBe(false);
+
+            //simulate another click - menu should be hidden again
+            menu.click();
+            //console.log("Simulated another mouseclick");
+            //bodyTag = document.getElementsByTagName('body')[0];
+            //console.log("Body class list equals: " + bodyTag.classList);
+            expect(bodyTag.classList.contains("menu-hidden")).toBe(true);
+          })
+        })
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe("Initial Entries", function () {
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -77,11 +106,98 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        beforeEach(function(done) {
+            loadFeed(0, function () {
+              done();
+            });
+        });
+
+         it("Entry added to feed container", function (done) {
+           let firstEntry = document.getElementsByClassName('entry-link')[0];
+           //console.log("Inside it statement, firstEntry is: " + firstEntry.textContent);
+           expect(firstEntry.textContent).not.toBe(undefined);
+           done();
+         })
+      });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
+    describe("New Feed Selection", function () {
+
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
-}());
+         let origFirstEntryText, newEntries;
+         /*loadFeed(0, function(done) {
+          origEntries = document.getElementsByClassName('entry-link');
+          //origFirstEntryText = new String(document.getElementsByClassName('entry-link')[0].textContent);
+        });*/
+
+        beforeEach(function (done) {
+          loadFeed(0, function () {
+            origFirstEntryText = document.getElementsByClassName('entry-link')[0].textContent;
+            //origEntries = document.getElementsByClassName('entry-link');
+            loadFeed(1, function () {
+              newEntries = document.getElementsByClassName('entry-link');
+              done();
+            })
+          })
+        });
+
+        it('LoadFeed function changes content', function () {
+          //let origFirstEntryText = new String(document.getElementsByClassName('entry-link')[0].textContent);
+          //console.log("Supposed constant is: " + origFirstEntryText);
+          /*loadFeed(0, function() {
+            console.log("Calling loadFeed at index 0 inside it block");
+            //origFirstEntryText = new String(document.getElementsByClassName('entry-link')[0].textContent);
+            console.log("Supposed constant is: " + origFirstEntryText);
+            //origEntries = document.getElementsByClassName('entry-link');
+            //origFirstEntryText = origEntries[0].textContent;
+            //console.log("\t" + origEntries[0].textContent);
+            //done();
+          })
+          loadFeed(1, function() {
+            console.log("Calling loadFeed at index 1 inside it block");
+            //newEntries = document.getElementsByClassName('entry-link');
+            //console.log("\tOld Entries text content " + origEntries[0].textContent);
+            console.log("\tNew Entries text content " + newEntries[0].textContent);
+            console.log("\tSupposed constant is: " + origFirstEntryText);
+            //done();
+            //expect(origFirstEntryText).not.toBe(newEntries[0].textContent);
+            //done();
+          })*/
+          console.log("After both loadfeeds, the original text is: " + origFirstEntryText);
+          console.log("\tAnd the new text is: " + newEntries[0].textContent);
+          expect(origFirstEntryText).not.toBe(newEntries[0].textContent);
+            //done();
+        })
+    });
+
+
+         /*let origEntries = document.getElementsByClassName('entry-link');
+
+         beforeEach(function(done) {
+           origEntries = document.getElementsByClassName('entry-link');
+           loadFeed(1, function () {
+             done();
+           });
+         });
+
+         it("Load feed functions properly", function () {
+           let newEntries = document.getElementsByClassName('entry-link');
+
+         // Does one exist?
+         expect(newEntries).not.toBe(undefined);
+
+         // and must match the appropriate title
+         expect( articleTitle.text() ).toEqual( allFeeds[ feedIndex ].name );
+
+         // TODO: >>>>>> but it must not be identical to the first feed <<<<<<<<
+         expect( articleTitle.text() ).not.toEqual( allFeeds[ originalFeedIndex ].name );
+
+         // must stop async
+         done();
+       })*/
+    //});
+}());//END OF ALL TESTING
